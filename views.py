7ea34b50e0b4276f9649f760e395127966af2b38
Harris Lapiroff <harris@chromamine.com>
2012-05-08 01:46:55 -0400

    Very basic views and templates.

--- views.py
+++ views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.views.generic import DetailView, ListView
+from art.models import Artist, Artwork
+
+
+class ArtistDetailView(DetailView):
+	model = Artist
+	
+	def get_object(self):
+		return self.model.objects.get(username=self.kwargs['username'])
+
+
+class ArtistListView(ListView):
+	model = Artist
+
+
+class ArtworkDetailView(DetailView):
+	model = Artwork
+
+
+artist_detail = ArtistDetailView.as_view()
+artist_list = ArtistListView.as_view()
+artwork_detail = ArtworkDetailView.as_view()
